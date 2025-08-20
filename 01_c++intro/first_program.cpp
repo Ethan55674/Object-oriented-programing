412102624eb1737224a2ecd64b1b956e8aafadd4
Ethan Kohlas <ekohlas0231@floridapoly.edu>
2025-08-20 18:40:20 +0000

    program_1

---
+++ 01_c++intro/first_program.cpp
@@ -0,0 +1,18 @@
+/*
+    08/20/25
+    the first program in C++
+*/
+#include <iostream>
+
+// create a main function
+
+int main(void){
+    std::cout << "Hello COP3337!" << std::endl;
+
+    // std - standard namespace
+    // :: - scope resolution operator
+    // cout console output (object of an iostream class)
+    // << stream insertion operator
+    // endl - end of line
+    return 0;
+}
